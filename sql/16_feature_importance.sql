16fe5305e214d17d9442753546ae16e7b2b31c65
Nikita Dongre <nikitadongre1430@gmail.com>
2026-04-06 09:55:21 -0700

    Delete sql directory

--- sql/16_feature_importance.sql
+++
@@ -1,6 +0,0 @@
--- Query 16: Feature Importance
--- Which factors most influence whether a customer will purchase?
-SELECT
-  *
-FROM ML.WEIGHTS(MODEL `analytics.purchase_predictor`)
-ORDER BY ABS(weight) DESC;
